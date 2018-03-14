76466a349e5e81fd8d57b0b31d4d4dd3a25f3b1d
joerg.krause <root@JOERGDELL.localdomain>
2018-03-14 22:20:45 +0100

    Removed dist

--- dist/app/configurations/service.actions.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ALL_USERS = 'ALL_USERS';
-exports.CURRENT_USER = 'CURRENT_USER';
-exports.SHOW_PHONE = 'SHOW_PHONE';
